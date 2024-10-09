119919a37eff5852aaa603f9df0e9bb0e3cc7b29
Dev-PeterWachira <wmainap@gmail.com>
2024-10-09 18:13:44 +0300

    object methods

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+let employee = {
+    name : "Maish",
+    streetAddress : "Ring Road"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+    ...employee, [key]:value
+    };
+}
+let newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Peter"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+  object[key] = value;
+  return object;
+}
+const updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Peter");
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedKey, ...newEmployee } = employee; 
+    return newEmployee; 
+}
+const newlyEmployed = deleteFromEmployeeByKey(employee, 'age');
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+const deletedKey = destructivelyDeleteFromEmployeeByKey(
+    employee,"name"
+);
